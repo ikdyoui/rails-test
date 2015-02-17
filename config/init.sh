645e7bb398752c0c87e6de7e00aa5473ef46f74d
Yuichi Ikeda <ikdyoui@gmail.com>
2015-02-17 14:08:47 +0900

    rm init.sh

--- config/init.sh
+++
@@ -1,6 +0,0 @@
-sudo su - postgres -c 'createuser rails-test'
-sudo su - postgres -c 'createdb rails-test_development'
-export RAILS_ENV=development
-bundle exec rake db:create 
-bundle exec rake db:migrate
-
